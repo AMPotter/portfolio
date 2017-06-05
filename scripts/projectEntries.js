0512611e190dbb8389545512a7f581c8fb8e036e
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-06-04 22:15:54 -0700

    add placeholder js objects and script files, add more html work

---
+++ scripts/projectEntries.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var projectData = [
+    {
+        title: 'Project One',
+        projectUrl: 'http://twitter.com/',
+        projectBlurb: '<p>This is the first project I worked on.</p>',
+    },
+    {
+        title: 'Project Two',
+        projectUrl: 'http://youtube.com/',
+        projectBlurb: '<p>This is the second project I worked on.</p>'
+    },
+    {
+        title: 'Project Three',
+        projectUrl: 'http://facebook.com/',
+        projectBlurb: '<p>This is the third project I worked on.</p>'
+    }
+];
