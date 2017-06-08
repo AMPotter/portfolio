e3370041fdafc93921fe2e59d13deccfe168edf3
Mark Petrie <petrie.mark@gmail.com>
2017-06-08 15:22:09 -0700

    Add Project constructor and toHtml function for rendering on index.html.

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,1 +1,17 @@
 'use strict';
+
+var projectView = {};
+
+projectView.handleMainNav = function () {
+    $('#main-nav li.tab').on('click', function() {
+        console.log('clicked!');
+        $('.tab-content').hide();
+        var clickedTab = $(this).data('content');
+        $('#' + clickedTab).show();
+    });
+    $('.main-nav .tab:first').click();
+};
+
+$(document).ready(function () {
+    projectView.handleMainNav();
+});
